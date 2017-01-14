3b9bbacbb4fb16431a9ea0b16a13bb841d49bd06
Paulo <pauloguedescamargo@gmail.com>
2017-01-14 20:21:31 -0200

    Improvement changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,4 @@
 $(document).ready(function() {
-  $('.countdown-timer').countdown('2017/01/13', function(event) {
-    $('.days', this).text(event.strftime('%D'));
-    $('.hours', this).text(event.strftime('%H'));
-    $('.minutes', this).text(event.strftime('%M'));
-    $('.seconds', this).text(event.strftime('%S'));
-  });
-
   $('nav').onePageNav({
     scrollOffset: 50,
     currentClass: 'active',
@@ -23,60 +16,6 @@ $(document).ready(function() {
       }, 500);
     }
   }
-
-  $('#btn-registration').click(function(e) {
-    e.preventDefault();
-    scrollTo('form');
-  });
-
-  $('#subscribe-form').on('submit', function(e) {
-    e.preventDefault();
-
-    // get captcha response
-    var recaptchaResp = grecaptcha.getResponse();
-
-    // validate captach
-    if (!recaptchaResp) {
-      alert('Marque Não Sou Robo para registrar');
-      return;
-    }
-    
-    // get data from form
-    var user = {}
-    $(this).find('input').each(function(i) {
-      var $input = $(this);
-      user[$input.attr('name')] = $input.val().trim();
-    });
-
-    // validate required
-    if (!user.nome || !user.email || !user.faculdade || !user.semestre) {
-      alert('Preencha os campos obrigatórios');
-      return;
-    }
-
-    if (!IsEmailValid(user.email)) {
-      alert('Email inválido');
-      return;
-    }
-
-    // request
-    $.ajax({
-      url: 'subscribe.php',
-      method: 'POST',
-      data: { recaptchaResponse : recaptchaResp, user: user }
-    }).done(function(result) {
-      if (result) {
-        alert(result);
-        if (result.indexOf('sucesso') >= 0) {
-          clearForm();
-        }
-      }
-    }).fail(function(err) {
-      console.log(err);
-    }).always(function() {
-      grecaptcha.reset();
-    });
-  });
 });
 
 function initMap() {
@@ -119,20 +58,4 @@ function initMap() {
     map.setCenter(buguer);
     map.panBy(0, 0);
   });
-}
-
-function clearForm() {
-  $('#subscribe-form input').each(function(i) {
-    var $input = $(this);
-    $input.val('');
-  });
-}
-
-function IsEmailValid(email) {
-  var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-  if (!regex.test(email)) {
-    return false;
-  }else{
-    return true;
-  }
 }
